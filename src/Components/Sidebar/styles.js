2f22af17a1e21df1f25abca8ccd47794482cef26
ValeriaStav <ValeriiaStavriadi@gmail.com>
2022-09-29 23:08:35 +0300

    feat: add launch guide

--- src/Components/Sidebar/styles.js
+++ src/Components/Sidebar/styles.js
@@ -1,48 +1,49 @@
-import styled from 'styled-components'
-import { NavLink } from 'react-router-dom'
-import { FlexRow } from '../../styles'
-import SideBarLogo from '../../assets/SideBarLogo.png'
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { FlexRow } from "../../styles";
+import SideBarLogo from "../../assets/SideBarLogo.png";
 
 export const Logo = styled.img`
-  width: 90%;
-  height: 17%;
-`
+    width: 90%;
+    margin: auto;
+`;
 
 export const Container = styled(FlexRow)`
-  width: 300px;
-  background-color: #333a3d;
-  height: 100vh;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-`
+    width: 300px;
+    background-color: #333a3d;
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    position: sticky;
+`;
 export const BottomContainer = styled(FlexRow)`
-  width: 300px;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  margin-top: 14%;
-`
+    width: 300px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    margin-top: 14%;
+`;
 
 export const StyledLink = styled(NavLink)`
-  padding: 10px;
-  margin-bottom: 7%;
-  color: white;
-  background-color: ${({active}) => active ? '#292F31' : 'transparent'};
-  text-decoration: none;
-  width: 68%;
-  justify-content: center;
-  font-weight: ${({active}) => active ? '900' : '500'};
-  cursor: pointer;
+    padding: 10px;
+    margin-bottom: 7%;
+    color: white;
+    background-color: ${({ active }) => (active ? "#292F31" : "transparent")};
+    text-decoration: none;
+    width: 68%;
+    justify-content: center;
+    font-weight: ${({ active }) => (active ? "900" : "500")};
+    cursor: pointer;
 
-  &:hover {
-    color: yellow;
-  }
+    &:hover {
+        color: yellow;
+    }
 
-  svg {
-    margin-right: 5px;
-  }
-`
+    svg {
+        margin-right: 5px;
+    }
+`;
 Logo.defaultProps = {
-  src: SideBarLogo,
+    src: SideBarLogo,
 };
