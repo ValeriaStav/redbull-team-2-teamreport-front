40db4bc74ab5b88d693022e6e7fad4976e87996e
Alexandra Melenteva <alexandram201578@gmail.com>
2022-10-17 11:45:47 +0200

    fix routing and invite your team

--- src/Pages/Invite Your Team/index.js
+++ src/Pages/Invite Your Team/index.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 import { AiFillCaretRight } from "react-icons/ai";
-import Sidebar from "../../Components/Sidebar";
+import { useDispatch, useSelector } from "react-redux"
+import { Formik, ErrorMessage } from "formik";
+import { useNavigate } from "react-router-dom";
+
 import Header from "../../Components/Header";
 import HelpButton from "../../Components/HelpButton";
 import FeedbackButton from "../../Components/FeedbackButton";
+import Error from "../../Components/Error"
+
 import { FlexCol } from "../../styles";
 import {
     HeaderText,
@@ -14,21 +18,24 @@ import {
     Button,
     Input,
 } from "./styles";
-import { Formik, ErrorMessage } from "formik";
-import { useNavigate } from "react-router-dom";
+
 import validate, {
+    startsFromCapitalize,
     moreThanTree,
     lessThanHundred,
     validEmail,
-    passwordRule,
 } from "../../utils/validators";
 
 const InviteYourTeam = () => {
     const [open, setOpen] = useState(false);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const teamId = useSelector((state) => state.usersReducer.currentUserCommand)
+
     const submit = (values) => {
-        navigate("/");
-    };
+        setOpen(true)
+        dispatch({ type: "SEND_INVITATION", payload: { ...values, teamId } })
+        };
     return (
         <FlexCol>
             <Header Content={<HeaderText>Invite Your Team</HeaderText>} />
@@ -49,24 +56,14 @@ const InviteYourTeam = () => {
                         </AccordionContent>
                     </AccordionItem>
                     <FormItem>
-                        <AccordionContent>
                             <p>
                                 <AiFillCaretRight />
                                 The team member didn't receive yout invite?
                             </p>
-                        </AccordionContent>
                     </FormItem>
                 </>
             )}
             <AccordionItem>
-                <AccordionContent>
-                    <strong>
-                        Enter the team member you would like to invite.
-                    </strong>
-                    <p>
-                        Don't worry! You'll be able to add more team members
-                        later.
-                    </p>
                     <Formik
                         onSubmit={submit}
                         initialValues={{
@@ -79,12 +76,12 @@ const InviteYourTeam = () => {
                                 {
                                     name: "firstName",
                                     value: values.firstName,
-                                    functions: [moreThanTree, lessThanHundred],
+                                    functions: [moreThanTree, lessThanHundred, startsFromCapitalize],
                                 },
                                 {
                                     name: "lastName",
                                     value: values.lastName,
-                                    functions: [moreThanTree, lessThanHundred],
+                                    functions: [moreThanTree, lessThanHundred, startsFromCapitalize],
                                 },
                                 {
                                     name: "email",
@@ -94,64 +91,57 @@ const InviteYourTeam = () => {
                             ])
                         }
                     >
-                        {({ values, handleSubmit, handleChange }) => (
-                            <form onSubmit={handleSubmit}>
-                                <label
-                                    for="exampleInputFirstName1"
-                                    class="form-label"
-                                >
-                                    <strong>First Name</strong>
-                                </label>
-                                <Input
-                                    type="text"
-                                    class="form-control"
-                                    aria-label="First name"
-                                    name="firstName"
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage
-                                    name="firstName"
-                                    component={Error}
-                                />
-                                <label
-                                    for="exampleInputLastName1"
-                                    class="form-label"
-                                >
-                                    <strong>Last Name</strong>
-                                </label>
-                                <Input
-                                    type="text"
-                                    class="form-control"
-                                    aria-label="Last name"
-                                    name="lastName"
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage
-                                    name="firstName"
-                                    component={Error}
-                                />
-                                <label
-                                    for="exampleInputEmail1"
-                                    class="form-label"
-                                >
-                                    <strong>Email</strong>
-                                </label>
-                                <Input
-                                    type="email"
-                                    class="form-control"
-                                    id="exampleInputEmail1"
-                                    aria-describedby="emailHelp"
-                                    name="email"
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage name="email" component={Error} />
-                            </form>
-                        )}
-                    </Formik>
-                    <Button onClick={() => setOpen(true)}>Invite</Button>
-                </AccordionContent>
+                        {(({ handleSubmit, handleChange }) => (
+                        <AccordionContent onSubmit={handleSubmit}>
+                            <strong>
+                                Enter the team member you would like to invite.
+                            </strong>
+                            <p>
+                                Don't worry! You'll be able to add more team members
+                                later.
+                            </p>
+                            <label for="exampleInputFirstName1" class="form-label">
+                                <strong>First Name</strong>
+                            </label>
+                            <Input
+                                type="text"
+                                class="form-control"
+                                name="firstName"
+                                onChange={handleChange}
+                                aria-label="First name"
+                            />
+                            <ErrorMessage name='firstName' component={Error} />
+                            <label for="exampleInputLastName1" class="form-label">
+                                <strong>Last Name</strong>
+                            </label>
+                            <Input
+                                type="text"
+                                class="form-control"
+                                aria-label="Last name"
+                                name="lastName"
+                                onChange={handleChange}
+                            />
+                            <ErrorMessage name='lastName' component={Error} />
+                            <label for="exampleInputEmail1" class="form-label">
+                                <strong>Email</strong>
+                            </label>
+                            <Input
+                                type="email"
+                                class="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                name="email"
+                                onChange={handleChange}
+                            />
+                            <ErrorMessage name='email' component={Error} />
+                            <Button type="submit">Invite</Button>
+
+                        </AccordionContent>
+                    ))}
+                </Formik>
+                   
             </AccordionItem>
         </FlexCol>
     );
-};
+                        };
 export default InviteYourTeam;
--- src/Routes/RoutesWithSideBar.js
+++ src/Routes/RoutesWithSideBar.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+
 import HomePage from "../Pages/Home Page";
 import InviteYourTeam from "../Pages/Invite Your Team";
 import TeamReports from "../Pages/Team Reports";
@@ -6,23 +7,28 @@ import SeeMembers from "../Pages/See Members";
 import MyCompany from "../Pages/My Company";
 import EditMember from "../Pages/Edit Member";
 import MyReports from "../Pages/My Reports";
-
 import MyProfile from "../Pages/My Profile";
 import Sidebar from '../Components/Sidebar'
-import Header from '../Components/Header'
 import FillOutReport from '../Pages/FillOutReport'
-import { FlexCol } from '../styles'
-import HelpButton from '../Components/HelpButton'
+
 import { ColumnContainer, Container } from './styles'
 
 
 const RoutesWithSideBar = () => {
 
+  const userToken = localStorage.getItem('userToken')
+
+  if (!userToken) {
+    return <Navigate to="/login" replace />
+  }
+
+
   return (
+
     <ColumnContainer className="RouterWrapper">
       <Sidebar />
       <Container>
-        <div style={{ paddingBottom: '100px', backgroundColor:' #f6f7f8'}}>
+        <div style={{ paddingBottom: '100px', backgroundColor: ' #f6f7f8' }}>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/invite" element={<InviteYourTeam />} />
